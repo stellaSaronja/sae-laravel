3b7a75c9f0a0f50499d000de5f378169c64dee80
g2 <g2@mxdt.at>
2022-02-02 20:29:06 +0100

    migrations

--- database/migrations/2022_02_02_190925_create_postings.php
+++ database/migrations/2022_02_02_190925_create_postings.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreatePostings extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        // == https://laravel.com/docs/8.x/migrations
+
+        Schema::create('postings', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->boolean('is_published')->default(false);
+            $table->unsignedInteger('like_count')->default(0);
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('postings');
     }
 }
--- database/migrations/2022_02_02_192225_update_postings_add_dislike.php
+++ database/migrations/2022_02_02_192225_update_postings_add_dislike.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class UpdatePostingsAddDislike extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('postings', function (Blueprint $table) {
+
+           $table->unsignedInteger('dislike_count')->default(0)->after('like_count');
         });
     }
 
@@ -27,6 +26,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('postings', function (Blueprint $table) {
+
+            $table->dropColumn('dislike_count');
+        });
     }
 }
